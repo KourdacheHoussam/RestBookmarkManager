083c7b491ec35f1bfb0b3c1f3841926aeea81cbe
HoussamKourdache <kourdache88@yahoo.fr>
2014-11-23 13:55:06 +0100

    Models Added

---
+++ RestBookmarkManager/src/main/java/fr/tp/houssam/bookmarkmanager/models/FavoriteBookmark.java
@@ -0,0 +1,29 @@
+/**
+ * <p>
+ * Copyright © 2014 Houssam KOURDACHE, France. All rights reserved.
+ * </p>
+ * <p>
+ * Ce document est la propriété de Houssam kourdache, France,
+ * il ne peut être ni reproduit, ni utilisé, ni communiqué, ni distribué
+ * à des tiers sans son autorisation préalable.
+ * </p>
+ * <p>
+ * Créé le 23 nov. 2014.
+ * </p>
+ */
+/**
+ * 
+ */
+package fr.tp.houssam.bookmarkmanager.models;
+
+/**
+ *
+ * @author Housssam
+ * @version 23 nov. 2014
+ */
+public class FavoriteBookmark {
+	private String id;
+	private String name;
+	private String description;
+}
+
