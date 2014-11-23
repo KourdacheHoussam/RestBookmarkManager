e322eb65f1e99fcfe8946e1584b07ed67831c544
HoussamKourdache <kourdache88@yahoo.fr>
2014-11-23 14:32:07 +0100

    JPA properties integrated

--- RestBookmarkManager/src/main/java/fr/tp/houssam/bookmarkmanager/models/BookMark.java
+++ RestBookmarkManager/src/main/java/fr/tp/houssam/bookmarkmanager/models/BookMark.java
@@ -16,17 +16,102 @@
  */
 package fr.tp.houssam.bookmarkmanager.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Generated;
+import javax.persistence.Column;
 /**
  *
  * @author Housssam
  * @version 23 nov. 2014
  */
+
+
+/**
+ * @Entity : i-e une entite jpa : jpa for hibernate config
+ * @Entity permet a BookMark de se faire mapper
+ **/
+
+@Entity 
 @XmlRootElement
+@Table(name="bookmarks")
 public class BookMark {
-	private int id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="bookmark_id")
+	private Integer id;
+	@Column(name="bookmark_name")
 	private String name;
-	private String type_repository;
+	@Column(name="bookmark_type")
+	private String type;
+	
+	
+	/** Il est impératif que Les entités JPA
+	 * se comporte comme les java beans
+	 * i-e avoir un constructuer vide sans paramètre
+	 * et des accesseurs/getters ainsi qu'un id pour identifier
+	 * l'élement au sein de la table */
+	
+	public BookMark(){}
+
+
+	/**
+	 *
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+
+	/**
+	 *
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	/**
+	 *
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 *
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	/**
+	 *
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+
+	/**
+	 *
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }
 
